0021799064993d97892073d571ef2cf7cd9381d2
Mostafa Moradian <mostafamoradian0@gmail.com>
2022-05-27 00:01:55 +0200

    Add script.js, README and deep-object-diff library as submodule

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # k6-diff-test
+
+This repository contains scripts to show how to create a baseline test and then compare the results with further tests (of the same script). It uses [deep-diff](https://github.com/flitbit/diff) and [deep-object-diff](https://github.com/mattphillips/deep-object-diff) libraries to determine the difference between your baseline test's end-of-test summary and your current one.
+
+I also use the [xk6-file](https://github.com/avitalique/xk6-file) extension to write the diffs into JSON files and you can build a k6 binary with this command:
+
+```bash
+xk6 build --with github.com/avitalique/xk6-file@latest
+```
+
+And then run two tests using this command.
+
+```bash
+./k6 run script.js
+```
+
+The first time the `baseline.json` doesn't exist, so the test will create it with the `handleSummary` function. The second time the `baseline.json` exists, so the `handleSummary` will compare the current test results against the baseline test and store two files:
+
+1) `diff.json`: contains the diff of two result summaries.
+2) `detailed-diff.json`: contains the diff of updated values in the second test's result summary against the baseline.
---
+++ script.js
@@ -0,0 +1,37 @@
+import http from "k6/http";
+import file from "k6/x/file";
+import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
+import deepdiff from "https://cdnjs.cloudflare.com/ajax/libs/deep-diff/1.0.2/deep-diff.min.js";
+import { detailedDiff } from "./dof.bundle.js";
+
+let baselineFile = null;
+
+try {
+    baselineFile = JSON.parse(open("baseline.json"));
+} catch (error) {
+    console.log("The 'base.json' is not found, so this test will be the baseline test");
+}
+
+export default function () {
+    http.get("https://test.k6.io");
+}
+
+export function handleSummary(data) {
+    console.log("Preparing the end-of-test summary...");
+
+    if (baselineFile !== null) {
+        console.log("Comparing the current test to the baseline...");
+        const jsonDiff = JSON.stringify(deepdiff.diff(baselineFile, data));
+        file.writeString("diff.json", jsonDiff);
+
+        console.log("Generate a detailed diff report...");
+        const jsonObjDiff = JSON.stringify(detailedDiff(baselineFile, data));
+        file.writeString("detailed-diff.json", jsonObjDiff);
+    }
+
+    let fileName = baselineFile === null ? "baseline.json" : "next.json";
+    return {
+        stdout: textSummary(data, { indent: " ", enableColors: true }),
+        [fileName]: JSON.stringify(data),
+    };
+}
